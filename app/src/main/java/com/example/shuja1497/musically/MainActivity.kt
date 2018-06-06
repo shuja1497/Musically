155ac81e1b4c929a8b0c066c173420ddeb5bb98b
shuja1497 <shujareshi1497@gmail.com>
2018-06-07 01:33:34 +0530

    layman logic on button click

--- app/src/main/java/com/example/shuja1497/musically/MainActivity.kt
+++ app/src/main/java/com/example/shuja1497/musically/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.shuja1497.musically
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClick(view: View){
+
+        when(view.id){
+
+            R.id.button_download->{
+                Toast.makeText(this, "Downloading", Toast.LENGTH_SHORT).show()
+                downloadSong()
+            }
+        }
+    }
+
+    private fun downloadSong() {
+        val endTime  = System.currentTimeMillis() + 10*1000
+
+        while (System.currentTimeMillis() < endTime){
+            Thread.sleep(1000)
+        }
+        Log.d(TAG, "Song downloaded")
     }
 }
