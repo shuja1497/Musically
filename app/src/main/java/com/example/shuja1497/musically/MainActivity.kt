7d25e19e45d3249c87eab7f1c0c9ad3f5a272b5f
shuja1497 <shujareshi1497@gmail.com>
2018-08-15 23:28:32 +0530

    checking

--- app/src/main/java/com/example/shuja1497/musically/MainActivity.kt
+++ app/src/main/java/com/example/shuja1497/musically/MainActivity.kt
@@ -48,6 +48,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+
         buttonPlay = findViewById(R.id.buttonPlay)
 
         button_download.setOnClickListener {
